4e9e4090abbfaf5753cda471b5dfdfb55ca866f2
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2025-03-16 12:17:44 +0100

    bluetooth audio support

--- src/whisperbox/audio/audio.py
+++ src/whisperbox/audio/audio.py
@@ -68,7 +68,7 @@ def _get_linux_loopback_device_index():
         for i in range(p.get_device_count()):
             device_info = p.get_device_info_by_index(i)
             device_name = str(device_info.get("name", "")).lower()
-            if any(name in device_name for name in ["pulse", "monitor", "loopback"]):
+            if any(name in device_name for name in ["pulse", "monitor", "loopback", "bluez", "monitor"]):
                 return i
         return None
     finally:
